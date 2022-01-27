d0f7eb807d4438cc21c87a38de9f84ca78198baa
joe-fennell <jtfennell@gmail.com>
2022-01-27 11:05:40 +0000

    doc update

--- build_lazydocs.sh
+++ build_lazydocs.sh
@@ -1,1 +1,4 @@
-lazydocs piccolo-lite --overview-file="API.md" --output-path="piccolo-lite/docs" --src-base-url="https://github.com/joe-fennell/piccolo-lite/"
+lazydocs piccolo-lite \
+--overview-file="API.md" \
+--output-path="piccolo-lite/docs"
+# --src-base-url="https://github.com/joe-fennell/piccolo-lite/blob/main/" \
